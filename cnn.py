624047d5566ea9a9495ecf8118a6cb857d260855
Siddharth <siddharth2000patel@gmail.com>
2023-05-09 01:40:14 +0530

    initial commit

--- cnn.py
+++ cnn.py
@@ -1,54 +1,32 @@
 import torch
 import torch.nn as nn
 
-class SimpleCNN(nn.Module):
-    def __init__(self):
-        super(SimpleCNN, self).__init__()
-        
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, stride=1, padding=1)
-        self.bn1 = nn.BatchNorm2d(16)
-        self.relu1 = nn.ReLU()
+class CNN(nn.Module):
+    def __init__(self, input_shape, num_classes):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=input_shape[2], out_channels=32, kernel_size=3, stride=1, padding=1)
         self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-        
-        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1)
-        self.bn2 = nn.BatchNorm2d(32)
-        self.relu2 = nn.ReLU()
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
         self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
-        
-        self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
-        self.bn3 = nn.BatchNorm2d(64)
-        self.relu3 = nn.ReLU()
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
         self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
-        
-        self.fc1 = nn.Linear(64 * 28 * 28, 512)
-        self.relu4 = nn.ReLU()
-        self.dropout1 = nn.Dropout(p=0.5)
-        
-        self.fc2 = nn.Linear(512, 10)
-        self.softmax = nn.Softmax(dim=1)
-        
+        self.fc1 = nn.Linear(in_features=128 * (input_shape[0] // 8) * (input_shape[1] // 8), out_features=512)
+        self.dropout = nn.Dropout(0.5)
+        self.fc2 = nn.Linear(in_features=512, out_features=num_classes)
+
     def forward(self, x):
         x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu1(x)
+        x = nn.functional.relu(x)
         x = self.pool1(x)
-        
         x = self.conv2(x)
-        x = self.bn2(x)
-        x = self.relu2(x)
+        x = nn.functional.relu(x)
         x = self.pool2(x)
-        
         x = self.conv3(x)
-        x = self.bn3(x)
-        x = self.relu3(x)
+        x = nn.functional.relu(x)
         x = self.pool3(x)
-        
-        x = x.view(-1, 64 * 28 * 28)
+        x = torch.flatten(x, 1)
         x = self.fc1(x)
-        x = self.relu4(x)
-        x = self.dropout1(x)
-        
+        x = nn.functional.relu(x)
+        x = self.dropout(x)
         x = self.fc2(x)
-        x = self.softmax(x)
-        
         return x
